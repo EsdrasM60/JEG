85d9cb7dbb858e9fb8a8ff8fabc101ac6e9897e0
emartinez <martinezeliezer@gmail.com>
2025-09-25 15:16:57 -0400

    chore(types): add heroicons module declarations to avoid TS build error on deploy

---
+++ src/types/heroicons.d.ts
@@ -0,0 +1,14 @@
+declare module '@heroicons/react/solid' {
+  const content: any;
+  export default content;
+}
+
+declare module '@heroicons/react/outline' {
+  const content: any;
+  export default content;
+}
+
+declare module '@heroicons/react/*' {
+  const content: any;
+  export default content;
+}
